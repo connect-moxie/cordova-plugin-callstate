e22b08b151f7a2ea7565bb4ba9c3c5db6f0725e0
kunwar <kunwarviveksingh@gmail.com>
2018-02-01 12:39:12 +0530

    Extended broadcastreceiver

--- src/android/CallState.java
+++ src/android/CallState.java
@@ -6,6 +6,7 @@ import org.apache.cordova.PluginResult;
 import android.content.Context;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.content.BroadcastReceiver;
 
 import org.json.JSONException;
 import org.json.JSONArray;
@@ -76,5 +77,32 @@ class CallStateListener extends PhoneStateListener {
         result.setKeepCallback(true);
 
         callbackContext.sendPluginResult(result);
+    }
+}
+public class PhoneStateReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+        // This is where you start your service        
+
+        try {
+            System.out.println("Receiver start");
+            String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+
+            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                Toast.makeText(context, "Incoming Call State", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "Ringing State Number is -" + incomingNumber, Toast.LENGTH_SHORT).show();
+            }
+            if ((state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))) {
+                Toast.makeText(context, "Call Received State", Toast.LENGTH_SHORT).show();
+            }
+            if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                Toast.makeText(context, "Call Idle State", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
